519905ded153b45aa17a5ee66a36301f883f8d87
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-11 22:41:08 +0200

    Решение 3 задачи

---
+++ Qwest3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a % 2) == 0)
+{
+    Console.WriteLine(a + " чётное число");
+}
+else
+{
+    Console.WriteLine(a + " не чётное число");
+}
