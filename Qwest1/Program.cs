6223df9a400a9febf0fcf38567d01c8c41077311
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-11 21:57:24 +0200

    Решение первой задачи

---
+++ Qwest1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b )
+{
+    Console.WriteLine (a + " это самое большое число ");
+    Console.WriteLine (b + " это самое маленькое число ");
+}
+if (b > a )
+{
+    Console.WriteLine (b + " это самое большое число");
+    Console.WriteLine (a + " это самое маленькое число ");
+}
