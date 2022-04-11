a2f749b21e692347a9f4a96f7085a413097dcfdb
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-11 22:31:53 +0200

    Решение 2ой задачи

---
+++ Qwest2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine(a + " максимальное число");
+}
+if (b > a && b > c)
+{
+    Console.WriteLine(b + " максимальное число");
+}
+if (c > a && c > b)
+{
+    Console.WriteLine(c + " максимальное число");
+}
+
